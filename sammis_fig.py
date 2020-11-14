c0e87911bd9b9a5c371463ff00fa5199831f9749
pbreslin <pbreslin@tcd.ie>
2020-11-14 16:42:49 +0000

    incorporated the custom dataframe

--- sammis_fig.py
+++ sammis_fig.py
@@ -6,7 +6,7 @@ pbreslin@tcd.ie
 ------------
 Last Update:
 ------------
-2020 November 10
+2020 November 14
 
 Description:
 ------------
@@ -22,54 +22,25 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 def main():
-    """ Loads the solar stats catalogue,
-        reorganises the dataframe and fixes some entries,
+    """ Loads the custom solar stats catalogue,
+        reorganises the dataframe,
         then creates GOES flux vs SRS sunspot group area plot
         (SAMMIS paper figure / Fig. 6 from HELCATS paper) """
 
     # Defining the path for the dataframe
-    dataframe = 'C:/Users/Peter/py_projects/solar_stats/cdaw_cme_flare_ar_smart_database.p'
+    custom_df = 'C:/Users/Peter/py_projects/solar_stats/custom_df.p'
 
     # Importing the dataframe
-    df = pd.read_pickle(dataframe)
+    df = pd.read_pickle(custom_df)
 
     # Looking at some flare props, particularly the outlier
-    dfs = df[['srs_hale', 'srs_area', 'flare_goes_class']]
+    dfs = df[['srs_hale', 'srs_area', 'goes_flux']]
 
     # Removing the rows containing any NaN values
     dfs = dfs.dropna(how='any')
 
     # Reo-rganising the indicies
     dfs = dfs.reset_index()
-
-    # Splitting goes_class column into 2 so as to separate the class from the flux value
-    goes_split = dfs.flare_goes_class.str.extract('([a-zA-Z]+)([^a-zA-Z]+)', expand=True)
-    goes_split.columns = ["goes_class", "goes_flux"]
-
-    # Adding these new columns into dfs
-    dfs['goes_class'] = goes_split['goes_class']
-    dfs['goes_flux'] = goes_split['goes_flux']
-
-    # Going to replace each incorrect value of 7,4 with 7.4
-    dfs['goes_flux'] = dfs['goes_flux'].replace(['7,4'], '7.4')
-
-    # Converting string flux values to a float
-    dfs['goes_flux'] = dfs['goes_flux'].astype(float)
-
-    # Now multiplying the flux values by the relevant power for its GOES class
-    dfs.loc[dfs.goes_class == 'A', 'goes_flux'] *= 1e-8
-    dfs.loc[dfs.goes_class == 'B', 'goes_flux'] *= 1e-7
-    dfs.loc[dfs.goes_class == 'C', 'goes_flux'] *= 1e-6
-    dfs.loc[dfs.goes_class == 'M', 'goes_flux'] *= 1e-5
-    dfs.loc[dfs.goes_class == 'X', 'goes_flux'] *= 1e-4
-
-    # Need to group Beta and BETA together, etc.
-    dfs["srs_hale"].replace({"ALPHA": "1", "Alpha": "1",
-                             "BETA": "2", "Beta": "2",
-                             "BETA-GAMMA": "3", "Beta-Gamma": "3",
-                             "BETA-DELTA": "4", "Beta-Delta": "4",
-                             "BETA-GAMMA-DELTA": "5", "Beta-Gamma-Delta": "5",
-                             "GAMMA-DELTA": "6", "Gamma-Delta": "6", }, inplace=True)
 
     # Converting string Hale values to a float
     dfs["srs_hale"] = dfs["srs_hale"].astype(float)
